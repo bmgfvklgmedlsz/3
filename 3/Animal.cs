0ec58e63764309c5aa6a80443a5a1955e2cef273
Blik-5 <svet26102001@gmail.com>
2025-03-24 11:25:42 +0300

    Добавьте файлы проекта.

---
+++ 3/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age) 
+        {
+            Name = name;
+            Age = age;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine($"имя: {Name}, возраст: {Age}");
+        }
+        public void voice ()
+        {
+            Console.WriteLine("мычание");
+        }
+    }
+
+}
---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal("муся", 8);
+            cow.ShowData();
+            cow.voice();
+            Console.ReadKey();
+        }
+    }
+}
